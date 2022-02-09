a9872e0536eddcef8e8b69e858ac85c657b1c8c7
mrdannyarmstrong <98234578+mrdannyarmstrong@users.noreply.github.com>
2022-02-09 15:54:57 -0500

    Add files via upload

---
+++ YenCon.py
@@ -0,0 +1,10 @@
+print ("Input USD")
+#convert USD into input
+u = input("$")
+#convert USD into a float
+a=float(u)
+#does U*109.47 and converts it into a float
+y = (a*109.762)
+#prints final anwser
+print ("¥", y)
+wait = input (" ")
